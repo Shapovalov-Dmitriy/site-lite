7d49c185aadaff212d30e821bce5e4032cd4195a
Dmitriy Shapovalov <shapovalov-84@internet.ru>
2023-12-11 15:48:33 +0700

    5 способов открыть меню

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
 "use strict";
+const burger = document.querySelector(".header__btn"),
+    nav = document.querySelector(".navigation");
+/*
+//  Способ №1
+if (burger) {
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("menu-active");
+    });
+}
+*/
+/*
+// Способ №2
+if (burger) {
+    burger.addEventListener("click", () => {
+        if (nav.classList.contains("menu-active")) {
+            nav.classList.remove("menu-active");
+        } else {
+            nav.classList.add("menu-active");
+        }
+    });
+}
+*/
+/*
+// Способ №3
+if (burger) {
+    burger.addEventListener("click", () => {
+        if (!nav.classList.contains("menu-active")) {
+            nav.classList.add("menu-active");
+        } else {
+            nav.classList.remove("menu-active");
+        }
+    });
+}
+*/
+/*
+// Способ №4 (наследование)
+if (burger) {
+    burger.addEventListener("click", () => {
+        if (nav.closest(".header__wrapper")) {
+            nav.classList.toggle("menu-active");
+        }
+    });
+}
+*/
+
+// Способ №5 (делегирование)
+document.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target.classList.contains("header__btn")) {
+        nav.classList.toggle("menu-active");
+    }
+});
